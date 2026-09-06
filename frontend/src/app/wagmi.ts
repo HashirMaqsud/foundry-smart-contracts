80a424dc7f62a50ee7f8f013a7433bae34738c6f
HashirMaqsud <rm.hashirmaqsud@gmail.com>
2026-09-07 00:04:45 +0500

    fix: hardcode fallback project id to eliminate qr border crash

--- frontend/src/app/wagmi.ts
+++ frontend/src/app/wagmi.ts
@@ -3,7 +3,7 @@ import { sepolia, anvil, mainnet } from 'wagmi/chains';
 
 export const config = getDefaultConfig({
   appName: 'Haxhir Web3 dApp',
-projectId: process.env.NEXT_PUBLIC_WALLETCONNECT_PROJECT_ID || '',
+  projectId: process.env.NEXT_PUBLIC_WALLETCONNECT_PROJECT_ID || '841954456e7a91cb59147dd04ecc5685',
   chains: [sepolia, anvil, mainnet],
   ssr: false,
 });
